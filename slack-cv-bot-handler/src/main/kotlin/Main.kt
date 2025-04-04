1a3ccfdfc70c9bbcfb49ac3dc703808d66ecc272
Roger A. Slaaen <roger@jpro.no>
2025-04-04 09:23:54 +0200

    Update call to openai with new model from flowcase

--- slack-cv-bot-handler/src/main/kotlin/Main.kt
+++ slack-cv-bot-handler/src/main/kotlin/Main.kt
@@ -31,6 +31,38 @@ private val promptFormatString = """
     %s
     </CV>
 """
+val summaryPromtFormatString =   """
+  <ROLLE>
+  Du er ekspert på å vurdere sammendrag av nøkkelkvalifikasjoner for CV skrevet av IT-konsulent.
+  </ROLLE>
+
+  <INNSTRUKS>
+  Du skal:
+  - Bruke oppgitte retningslinjer og kriterier mellom <RETNINGSLINJER> og </RETNINGSLINJER> for å vurdere tekst mellom <SAMMENDRAG> og </SAMMENDRAG>. Fokuser på konstruktiv tilbakemelding 
+  til bruker, du skal begrunne dine vurderinger med utgangspunkt i retningslinjer og gi konkrete forslag til forbedringer. 
+  - Lage et oppdatert sammendrag med referanser til erfarenheter fra prosjekter konsulenten har jobbet på. Bruk informasjonen mellom <PROSJEKTBESKRIVELSER> og </PROSJEKTBESKRIVELSER>.
+  </INNSTRUKS>
+  
+  <RETNINGSLINJER>
+  Sammendraget skal oppsummere konsulentens kompetanse, erfaring og personlige egenskaper. Sammendraget skal gi innsikt 
+  i hvem konsulenten er, dennes bakgrunn og hva konsulenten kan bidra med. Sammendraget er konsulentens 'elevator pitch'. 
+  Hvis konsulenten har 5, 10 eller 15 års erfaring, skal sammendraget være mer enn et par setninger. Det kan deles likt mellom: 
+  - Tekniske ferdigheter: Konsulenten skal beskrive sine viktigste tekniske ferdigheter og løfte frem erfaringer, viktige roller og ansvar 
+  denne har hatt. 
+  - Team/organisering/metodikk: Konsulentens erfaringer i tverrfaglige team, med organisering og metodikk skal beskrives. Konsulenten burde nevne hvordan 
+  denne beriker team/menneskene rundt seg og beskrive sitt verdibidraget. Det bør vises til både selvstendighet og samspill med andre 
+  (også andre fagdisipliner). 
+  - Personlige egenskaper: Konsulenten skal si noe om hva denne er spesielt engasjert i/opptatt av/interessert i.
+  </RETNINGSLINJER>
+  
+  <SAMMENDRAG>
+  %s
+  </SAMMENDRAG>
+  <PROSJEKTBESKRIVELSER>
+  %s
+  </PROSJEKTBESKRIVELSER>
+  """
+
 private const val CUSTOM_EVENT_TYPE = "cv_lest"
 private const val OPENAI_THREAD = "openai_thread_id"
 
@@ -63,14 +95,18 @@ fun handleCommand(slackSlashCommand: SlackSlashCommand) {
         it.threadTs(slackSlashCommand.threadTs)
             .text("Henter CV fra Flowcase")
     }
-    val cv = cvReader.readCV(slackSlashCommand.userEmail)
-    val jsonCV = objectMapper.writeValueAsString(cv)
+    val cv = cvReader.readCV(slackSlashCommand.userEmail)//TODO: what if cv not found
+
+    val summary = cv.key_qualifications.find { !it.disabled }?.long_description?.no?:""//TODO: what if no summary
+    val projects = cv.project_experiences.map { "<PROSJEKTBESKRIVELSE><PROSJEKT>${it.customer.no} - ${it.description.no} (fra: ${it.month_from}.${it.year_from} til: ${it.month_to}.${it.year_to})</PROSJEKT><BESKRIVELSE>${it.long_description.no?:""}</BESKRIVELSE></PROSJEKTBESKRIVELSE>" }.joinToString()
+
     slack.chatPostMessage {
         it.threadTs(slackSlashCommand.threadTs)
-            .text("Sender CV til OpenAI for vurdering")
+            .text("Sender sammendrag til OpenAI for vurdering")
     }
+
     openAIClient.startNewThread(
-        message = String.format(promptFormatString, jsonCV),
+        message = String.format(summaryPromtFormatString, summary, projects),
         onAnswer = { answer, openAiThread ->
             slack.chatPostMessage {
                 it
